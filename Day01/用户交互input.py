6d37de8958bf442884cfc01c5f06891d4694207c
JiangHuYiXiao <1163270704@qq.com>
2018-10-17 07:19:16 +0800

    input输出的一定是字符串

--- Day01/用户交互input.py
+++ Day01/用户交互input.py
@@ -1,5 +1,7 @@
-# -*- coding:utf-8 -*-
+#-*- coding:utf-8 -*-
 # 用户交互：获取用户的输入，然后进行输出，使用input()
+# input输出输出的数据全部是字符串
+
 name = input('请输入你的名字：')
 age = input('请输入你的年龄：')
 print('我的名字是：' + name,'我的年龄是：'+ age + '岁')
