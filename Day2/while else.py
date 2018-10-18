9a15cb01397479faff442b4e0b33a250638bba22
JiangHuYiXiao <1163270704@qq.com>
2018-10-18 19:46:52 +0800

    while else 如果循环没有被打断则执行else，打断了则不执行else

---
+++ Day2/while else.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+'''
+while 条件：
+    循环体
+    终止循环
+else：
+while
+else  解释：当循环体中没有被break打断则会运行else，打断则不会运行else
+'''
+# 例子
+a = 0
+while a < 5:
+    print(a)
+    a += 1
+    break
+else:
+    print('循环结束')  # 0
