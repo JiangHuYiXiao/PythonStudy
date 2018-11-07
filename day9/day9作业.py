700dcc3e8a12518bce0e621d41d9462f46986afb
JiangHuYiXiao <1163270704@qq.com>
2018-11-07 20:03:59 +0800

    day9作业

--- day9/day9作业.py
+++ day9/day9作业.py
@@ -24,6 +24,11 @@ def func(args):
         if index % 2 == 1:
             list.append(i)
     return list
+
+# 方法3：
+def func(args):
+    return args[1::2]
+
 # 调用函数
 res = func((2,3,1,5,6,9,4))
 print(res)
@@ -33,6 +38,7 @@ print(res)
 '''
 # 2、写函数，判断用户传入的对象（字符串，列表，元组）长度是否大于5
 '''
+# 方法1：
 def func2(args):
     k = 0
     for i in args:
@@ -48,10 +54,16 @@ res4 = func2([1,2,3,4,5,6])
 res5 = func2((1,2,3,4,5,6,7))
 print(res1,res2,res3,res4,res5)
 
+# 方法2：
+def func(args):
+    return(len(args) > 5)   #这里尽量都用return不用print或者break，这样因为有返回值，我们可以再对返回值做操作
+print(func('abcd'))
+if func('abcdd6'):
+    print('对象的长度确实大于5')
 '''
-
 # 3、写函数，检查传入列表的长度，如果大于2，那么保留前两个长度的内容，并将新内容返回给调用者。
 '''
+# 方法1
 def func3(list):
     lis = []
     k = 0
@@ -68,7 +80,19 @@ def func3(list):
 res1 = func3([113,12,33,44])
 res2 = func3([113,12])
 print(res1,res2)
+
+# 方法2:*************简化版***************
+def func(li):
+    if len(li) >2:
+        return li[:2]
+print(func([1,2,3,4]))
+
+# 方法2:*************精华版*************
+def func(li):
+        return li[:2]
+print(func([1,2,3,4]))
 '''
+
 # 4、写函数，计算传入字符串中的数字，字母，空格，以及其他的个数，并返回结果。
 '''
 def func4(arg):
@@ -104,19 +128,20 @@ print(func5([1,3, '',32]))
 
 # 6、写函数，检查传入字典的每一个value的长度，如果大于2，那么保留前两个长度的内容，并将新内容返回给调用者。
 # 字典的value值只能是字符串或者列表
-
-# def func6(args):
-#     dic = {'k1': 'v1v2', 'k2': [11, 22, 33, 44]}
-#     list = []
-#     for i in dic.values():
-#         if len(i) > 2:
-#             list.append(i[0])
-#             list.append(i[1])
-#     return list
-# res = func6({'k1': 'v1v2', 'k2': [11, 22, 33, 44]})
-# print(res)
-
+'''
+def func6(args):
+    dic = {'k1': 'v1v2', 'k2': [11, 22, 33, 44]}
+    list = []
+    for i in dic.values():
+        if len(i) > 2:
+            list.append(i[0])
+            list.append(i[1])
+    return list
+res = func6({'k1': 'v1v2', 'k2': [11, 22, 33, 44]})
+print(res)
+'''
 # 7、接收两个数字参数，返回较大的数
+'''
 # def compare_large(a,b):
 #     if a > b:
 #         return a
@@ -125,7 +150,7 @@ print(func5([1,3, '',32]))
 #     else:
 #         print('两个数一样大')
 # print(compare_large(2,1))
-
+'''
 
 # 8、写函数，用户传入修改的文件名，与要修改的内容，执行函数，完成整个文件的批量修改操作。
 
@@ -138,61 +163,62 @@ print(func5([1,3, '',32]))
 # os.remove('name')
 # os.replace('replace_name','name')
 
-# 9、写一个函数，完成三次注册，再写一个函数完成三次登录
 
+# 9、写一个函数，完成三次注册，再写一个函数完成三次登录
+'''
 # 1、定义函数
-# def registe():
-#     sum_digit = 0
-#     sum_lower = 0
-#     sum_upper = 0
-#     for i in range(3):
-#
-#         # 2、输入用户名
-#         name = input('请输入用户名:用户名必须包含数字、小写字母、大写字母且数字不能开头，字符长度为8-16个字符')
-#         for i in name:
-#             if i.isupper():
-#                 sum_upper += 1
-#             if i.islower():
-#                 sum_lower += 1
-#             if i.isdigit():
-#                 sum_digit += 1
-#         if (sum_digit <=0 or sum_digit > 6) or (sum_lower <=0 or sum_digit > 6) or (sum_upper <=0 or sum_digit > 6):
-#             print('用户名必须包含数字、小写字母、大写字母且数字不能开头，字符长度为8 - 16个字符')
-#             break
-#         if name[0].isdigit():
-#             print('数字不能开头，请重新输入用户名！')
-#             break
-#         if len(name) < 8 or len(name) > 16:
-#             print('用户名字符长度为8-16个字符，请重新输入用户名！')
-#             break
-#
-#         #3、输入密码
-#         password = input('请输入密码:密码必须包含数字、小写字母、大写字母且数字不能开头，字符长度为6-8个字符')
-#         for i in password:
-#             if i.isupper():
-#                 sum_upper += 1
-#             if i.islower():
-#                 sum_lower += 1
-#             if i.isdigit():
-#                 sum_digit += 1
-#         if (sum_digit <=0 or sum_digit > 6) or (sum_lower <=0 or sum_digit > 6) or (sum_upper <=0 or sum_digit > 6):
-#             print('密码必须包含数字、小写字母、大写字母且数字不能开头，请重新输入密码！')
-#             break
-#         if password[0].isdigit():
-#             print('数字不能开头，请重新输入密码！')
-#             break
-#         if (len(password) < 6) or (len(password) > 8):
-#             print('密码长度为6-8个字符，请重新输入密码！')
-#             break
-#         # 4、注册成功
-#         else:
-#             print('恭喜你，注册成功！')
-#
-#         # 5、将用户名，密码写入注册信息文件中，分行写入
-#         with open('注册信息',mode='w+',encoding='utf-8') as file:
-#             file.write('{}\n{}'.format(name,password))
-#         return
-# registe()
+def registe():
+    sum_digit = 0
+    sum_lower = 0
+    sum_upper = 0
+    for i in range(3):
+
+        # 2、输入用户名
+        name = input('请输入用户名:用户名必须包含数字、小写字母、大写字母且数字不能开头，字符长度为8-16个字符')
+        for i in name:
+            if i.isupper():
+                sum_upper += 1
+            if i.islower():
+                sum_lower += 1
+            if i.isdigit():
+                sum_digit += 1
+        if (sum_digit <=0 or sum_digit > 6) or (sum_lower <=0 or sum_digit > 6) or (sum_upper <=0 or sum_digit > 6):
+            print('用户名必须包含数字、小写字母、大写字母且数字不能开头，字符长度为8 - 16个字符')
+            break
+        if name[0].isdigit():
+            print('数字不能开头，请重新输入用户名！')
+            break
+        if len(name) < 8 or len(name) > 16:
+            print('用户名字符长度为8-16个字符，请重新输入用户名！')
+            break
+
+        #3、输入密码
+        password = input('请输入密码:密码必须包含数字、小写字母、大写字母且数字不能开头，字符长度为6-8个字符')
+        for i in password:
+            if i.isupper():
+                sum_upper += 1
+            if i.islower():
+                sum_lower += 1
+            if i.isdigit():
+                sum_digit += 1
+        if (sum_digit <=0 or sum_digit > 6) or (sum_lower <=0 or sum_digit > 6) or (sum_upper <=0 or sum_digit > 6):
+            print('密码必须包含数字、小写字母、大写字母且数字不能开头，请重新输入密码！')
+            break
+        if password[0].isdigit():
+            print('数字不能开头，请重新输入密码！')
+            break
+        if (len(password) < 6) or (len(password) > 8):
+            print('密码长度为6-8个字符，请重新输入密码！')
+            break
+        # 4、注册成功
+        else:
+            print('恭喜你，注册成功！')
+
+        # 5、将用户名，密码写入注册信息文件中，分行写入
+        with open('注册信息',mode='w+',encoding='utf-8') as file:
+            file.write('{}\n{}'.format(name,password))
+        return
+registe()
 
 def login():
     for i in range(3):
@@ -209,3 +235,4 @@ def login():
                 print('用户名或者密码错误，请重新登录！')
     print('三次机会已经用完，请明天再来！')
 login()
+'''
