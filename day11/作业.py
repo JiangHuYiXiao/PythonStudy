f656b9f0b71aca5c60b94697ba5f9c673a7c609e
JiangHuYiXiao <1163270704@qq.com>
2018-11-13 12:50:47 +0800

    作业

--- day11/作业.py
+++ day11/作业.py
@@ -30,7 +30,7 @@ print(res)
 #-*- coding:utf-8 -*-
 # 1、编写装饰器，为多个函数添加上认证的功能，
 # 要求登录成功一次，后续函数都无需再输入用户名和密码
-'''
+
 flag = False
 def wrapper(func):
     def inner(*args, **kwargs):
@@ -61,11 +61,11 @@ res = login()
 print(res)
 res1 = delete()
 print(res1)
-'''
+
 
 
 # 2、编写装饰器，为多个函数加上记录调用功能，要求每次调用函数都将被调用函数的名称写入文件
-'''
+
 def log(func):
     def inner(*args,**kwargs):
         res = func(*args,**kwargs)   # 被装饰的函数
@@ -89,14 +89,14 @@ print(res)
 
 res1 = delete(2)
 print(res1)
-'''
+
 
 
 # 3、进阶作业：
 # 1、编写一个下载网页内容的函数，要求功能是，用户传入一个URL，函数返回下载页面的结果
 # 2、为题目1编写装饰器，实现缓存网页内容的功能
 # 具体：实现下载的页面，存放于文件中，如果文件内有值（文件大小不为0），就优先从文件中读取网页内容，否则就去下载
-'''
+
 # 1、
 # 方法1
 def get_url(url):
@@ -117,27 +117,33 @@ def get_url(url):
 
 res = get_url('http://www.baidu.com')
 print(res)            # <bound method HTTPResponse.read of <http.client.HTTPResponse object at 0x0000000002ED70F0>>
-'''
+
 
 # 2、
-def cache(*args,**kwargs):
-    def inner(func):
-        with open('cache',mode='wb+') as file:
-            if file.__sizeof__() > 0:
-                return file.read
-            else:
-                ret = get_url(func)
-                file.write(ret)
-                return ret
+import os
+from urllib.request import urlopen
+def cache(func):
+    def inner(*args,**kwargs):
+        if os.path.getsize('web_cache1'):
+            with open('web_cache1', mode='rb') as file:
+                return file.read()
+        else:
+            ret = func(*args,**kwargs)
+            with open('web_cache1',mode='wb') as file:
+                file.write(b'*****'+ ret)
+            return ret
     return inner
 
 @cache
 def get_url(url):
-    from urllib.request import urlopen
-    code = urlopen(url).read
+    code = urlopen(url).read()
     return code
 
-
 res = get_url('http://www.baidu.com')
-print(res)            # <bound method HTTPResponse.read of <http.client.HTTPResponse object at 0x0000000002ED70F0>>
+print(res)
+res = get_url('http://www.baidu.com')
+print(res)
+
+# 一定要注意web_cache文件不能删除了然后再继续调用，这样再次写入会写入不了
+
 
