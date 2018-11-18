e0fc018784c7ae5fc1dafe22b0078bc0c9b4f2e4
JiangHuYiXiao <1163270704@qq.com>
2018-11-18 09:12:36 +0800

    day12复习，函数，装饰器

--- day13/复习.py
+++ day13/复习.py
@@ -87,3 +87,239 @@ func(*L)     # 站在实参的角度，给L加上*就是按照顺序打散这个
     # 函数内部定义的变量，函数名等，在函数调用结束而结束
     # 每个函数都有自己的局部命名空间，互相之间不共用
 
+# 5、作用域
+# 全局作用域：就是全局命名空间的，全局命名的变量在全局可以使用
+# 局部作用域：局部命名空间的就是局部作用域，默认只可以在局部使用
+
+# 6、global
+# 如果想让局部命名空间的变量可以作用于全局，可以使用global在变量前来修饰变量
+# a = 10
+#
+# def inner():
+#     global a
+#     a = 20
+#     print(a)
+# print(a)
+# inner()
+# print(a)
+
+
+# 7、nonlocal
+# 如果想在内部改变外部变量的值，可以使用nonlocal进行修饰变量
+# def outer():
+#     a  = 1
+#     def inner():
+#         nonlocal a
+#         a = 2
+#         print('inner',a)
+#     inner()
+#     print('outer',a)
+# outer()
+
+# 8、locals(),globals()分别返回函数的局部变量和全局变量
+# def outer():
+#     c = 100
+#     def inner():
+#         a = 12
+#         b = 20
+#         print(locals())
+#         print(globals())
+#     inner()
+# outer()
+# print(globals())
+# print(locals())   #放在全局返回全局变量
+
+
+# 9、函数调用实质是内存地址（）只是因为内存地址是16进制，解释器帮我们转换为str
+
+
+# 10、函数的嵌套
+# 嵌套定义
+# def func1():
+#     a = 1
+#     func1()
+#     b = 2
+
+
+# 嵌套调用
+# def func1():
+# #     a = 2
+# #     def func2():
+# #         func1()  # 嵌套调用func1
+
+
+# 11、函数名
+#     1、其实就是函数的内存地址
+#     2、可以作为返回值
+#     3、可以作为参数传递
+#     4、可以作为容器
+#     5、可以赋值
+
+# 12、闭包
+# 闭包必须是函数嵌套，且内部函数有使用外部函数的变量
+# def outer():
+#     a = 10
+#     def inner():
+#         b = a + 10
+#         print('b:',b)
+#     inner()
+#     print(inner.__closure__)   # 判断函数是否为闭包函数
+#     print('a:',a)
+#
+# outer()
+
+
+# 13、函数外部使用内部函数变量，两种方法
+#   1、内部函数使用global
+#   2、内部函数返回函数名
+# def outer():
+#     print('outerfunction')
+#     def inner():
+#         a = 10
+#         print('innerfunction')
+#         return a
+#     return inner
+#
+#
+# res = outer()  # 此时res就是inner函数名，所以可以直接res()
+# result = res()
+# print(result)
+
+# 14、闭包函数的应用，---装饰器
+# 固定格式
+
+# def wrapper(func):
+#
+#     '被装饰函数之前可以添加的功能'
+#     def inner(*args,**kwargs):
+#         '被装饰函数之后可以添加的功能'
+#         res = func(args,kwargs)
+#         return res
+#     return inner
+#
+# @wrapper
+# def func(*args, **kwargs):
+#     print('你们好')
+#
+# # 调用
+# # func = wrapper(func)
+# func()
+
+
+
+
+
+# def wrapper(func):
+#     # '被装饰函数之前可以添加的功能'
+#     def inner(*args, **kwargs):
+#         # '被装饰函数之后可以添加的功能'
+#         print('大家好')
+#         res = func(args, kwargs)
+#         return res
+#     return inner
+#
+# @wrapper
+# def func(*args, **kwargs):
+#     print('你们好')
+#
+# # func = wrapper(func)
+# func()
+
+
+# 14、装饰器遵循一个原则：开放封闭原则
+    # 对扩展是开放的
+    # 对修改是封闭的
+
+# 15、带参数的装饰器
+import time
+# flag = False
+# flag = True
+# def outer(flag):
+#     def timer(func):
+#         def inner(*args,**kwargs):
+#             if flag:
+#                 start = time.time()
+#                 re = func(*args,**kwargs)
+#                 end = time.time()
+#                 print(end - start)
+#                 return re
+#             else:
+#                 re = func(*args, **kwargs)
+#                 return re
+#         return inner
+#     return timer
+
+# @outer相当于下面这两句
+# timer = outer(timer)   先是调用outer返回timer，
+# @timer  然后再@timer进行装饰,说到底outer函数只是传了个参数过来,所以叫做带参数的装饰器
+
+# @outer(flag)
+# def wahaha():
+#     time.sleep(0.01)
+#     print('wahahaha')
+#
+# @outer(flag)
+# def jianghu():
+#     time.sleep(0.01)
+#     print('jianghuhu')
+#
+#
+# wahaha()
+# jianghu()
+
+
+# 16、多个装饰器装饰一个函数
+
+# def wrapper1(func):
+#     def inner2():
+#         print('wrapper1 ,before func')      # 1
+#         func()
+#         print('wrapper1 ,after func')       # 7
+#     return inner2
+#
+# def wrapper2(func):
+#     def inner2():
+#         print('wrapper2 ,before func')      # 2
+#         func()
+#         print('wrapper2 ,after func')       # 6
+#     return inner2
+#
+# def wrapper3(func):
+#     def inner3():
+#         print('wrapper3 ,before func')      # 3
+#         func()
+#         print('wrapper3 ,after func')       # 5
+#     return inner3
+#
+#
+# @wrapper1
+# @wrapper2
+# @wrapper3
+# def f():
+#     print('in f')   # 4
+#
+# f()
+
+
+# 17、想要获取装饰器的注释，内置函数，使用wraps
+from functools import wraps
+def wrapper(func):
+    # '被装饰函数之前可以添加的功能'
+    @wraps(func)
+    def inner(*args, **kwargs):
+        '''这个是inner函数'''
+        # '被装饰函数之后可以添加的功能'
+        print('大家好')
+        res = func(args, kwargs)
+        return res
+    return inner
+
+@wrapper
+def func(*args, **kwargs):
+    '''这个是函数'''
+    print('你们好')
+
+print(func.__name__)
+print(func.__doc__)
+
+func()
