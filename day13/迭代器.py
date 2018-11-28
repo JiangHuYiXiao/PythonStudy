06754ea0306f1ec7166c09937a898fb60d74ee0c
JiangHuYiXiao <1163270704@qq.com>
2018-11-28 08:13:40 +0800

    迭代器

---
+++ day13/迭代器.py
@@ -0,0 +1,16 @@
+#-*- coding:utf-8 -*-
+# 学习到目前为止，我们所学过的可以被for循环的有：这些其实都是可迭代对象
+'''
+1、字符串，str
+2、列表，list
+3、枚举，enumerate,返回索引和值，针对于所有可迭代对象
+a = 'jsjdff'
+for i in enumerate(a):
+    print(i)
+
+4、字典，dict
+5、元组，tuple
+6、集合，set
+7、range()
+8、file，文件句柄
+'''
