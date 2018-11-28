567994d3e37d89c65f30f2e2fc84a8827c0a40c5
JiangHuYiXiao <1163270704@qq.com>
2018-11-28 20:04:08 +0800

    生成器

--- day13/生成器.py
+++ day13/生成器.py
@@ -20,11 +20,12 @@ print(res)
 # yield必须在函数内部，且一个函数里面不能同时含有yield和return
 # 生成器函数执行后，会返回一个生成器作为返回值
 # yield也可以返回函数值，但是yield之后不会结束函数，return会结束函数
+'''
 def generator():  # 步骤1
     print(1)      # 步骤6
     yield 'a'     # 步骤7
 
-res = generator()   # 没有返回值 右边：# 步骤2 左边：# 步骤3
+res = generator()   # 返回一个生成器 右边：# 步骤2 左边：# 步骤3
 print(res)   # <generator object generator at 0x00000000022B3750> # 步骤4
 # 判断生成器函数generator()是否为迭代器
 # from collections import Iterator
@@ -34,5 +35,76 @@ print(res)   # <generator object generator at 0x00000000022B3750> # 步骤4
 # 这样的函数内部包含yield关键字的都叫做生成器函数，这种函数调用需要通过迭代器去调用，而生成器函数的本质就是迭代器
 # 所以可以直接调用生成器函数的__next__(）方法
 print(res.__next__())   # 步骤5
+'''
+# 连续取生成器函数的值
+# def generator():
+#     print(1)
+#     yield 'a'
+#     print(2)
+#     yield 'b'
+#     print(3)
+#     yield 'c'
+#
+# res = generator()
+# print(res.__next__()) # 1,a
+# print(res.__next__()) # 2,b
+# print(res.__next__()) # 3,c
+# print(res.__next__()) # StopIteration
+
+# yield就不会结束函数，会继续取值
+
+# for循环取全部生成器函数的值
+# def generator():
+#     print(1)
+#     yield 'a'
+#     print(2)
+#     yield 'b'
+#     print(3)
+#     yield 'c'
+# res = generator()
+# for i in res:
+#     print(i)  #1,a,2,b,3,c
+
+
+# 需求，生成wahaha+序列（200000）
+# def func():    # 生成器函数
+#     for i in range(200001):
+#         yield 'wahaha%s'%i
+#
+# res = func()   # 生成器
+# for i in res:
+#     print(i)
+
+
+# 只想要取得50个
+def func():    # 生成器函数
+
+    for i in range(200001):
+        yield 'wahaha%s'%i
+
+res = func()   # 生成器
+count = 0
+for i in res:
+    count += 1
+    print(i)
+    if count > 50:
+        break
+
+# 还可以接着取值
+print('****',res.__next__())
+
+# 还可以接着取值
+for i in res:
+    count += 1
+    print(i)
+    if count > 100:
+        break
+
+
+# 迭代器和可迭代的关于for循环的差异
+
+
+
+
 
 
