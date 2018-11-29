917c2730cd5f3e4879361afd05e00c7a564a848e
JiangHuYiXiao <1163270704@qq.com>
2018-11-29 14:45:08 +0800

    监听文件输入的例子

--- day13/监听文件输入的例子.py
+++ day13/监听文件输入的例子.py
@@ -1,6 +1,44 @@
 # -*- coding:utf-8 -*-
-f = open('file',encoding = 'utf-8')
-while True:
-    line = f.readline()
-    if line:
-        print(line)
+#1、 简单的文件监听的例子：
+
+def listener(filename):
+    with open(filename,mode = 'r',encoding = 'utf-8') as file:
+        while True:
+            line = file.readline()
+            if line.strip():
+                print(line.strip())
+
+listener()
+
+
+# 2、如果要求前面2行是要加上***，后面的行加上###，这个函数是无法满足的
+def listener(filename):
+    with open(filename,mode='r',encoding='utf-8') as file:
+        while True:
+            line = file.readline()
+            if line.strip():
+                yield(line.strip())
+
+res = listener('file1')
+count = 0
+for i in res:
+
+    if count < 2:
+        print('***',i)
+    else:
+        print(i,'###')
+    count += 1
+
+# 3、如果文件中行包含有python则打印，做到一个监听过滤的功能
+def listener(filename):
+    with open(filename,mode='r',encoding='utf-8') as file:
+        while True:
+            line = file.readline()
+            if line.strip():
+                yield(line.strip())
+
+res = listener('file1')
+
+for i in res:
+    if 'python' in i:
+        print(i)
