6b57504206c61fca8558d1b6c3a474b5b3fb3b23
JiangHuYiXiao <1163270704@qq.com>
2019-01-23 07:59:37 +0800

    模块导入

--- day20/demo.py
+++ day20/demo.py
@@ -1,6 +1,10 @@
 # -*- coding:utf-8 -*-
-_all_ = ['read']    # 只有money变量可以使用
+# _all_ = ['read']    # 只有money变量可以使用
 print('in demo.py')
 money = 10000
 def read():
     print('in read',money)
+
+if __name__ == '__main__':
+    print('run the demo')
+print(__name__)         #__main__
