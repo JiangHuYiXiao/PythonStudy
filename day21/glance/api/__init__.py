cb1861cdb34f748190bd75d148cdfe24dd01bb19
JiangHuYiXiao <1163270704@qq.com>
2019-01-23 20:01:21 +0800

    file

--- day21/glance/api/__init__.py
+++ day21/glance/api/__init__.py
@@ -1,5 +1,12 @@
-# import glance.api.policy
-# import glance.api.versions
+'''
+绝对路径：
+import glance.api.policy
+import glance.api.versions
 # 或者
 from glance.api import policy
 from glance.api import versions
+
+'''
+# 相对路径：
+from . import policy
+from . import versions
