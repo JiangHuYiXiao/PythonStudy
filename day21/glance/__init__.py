6936a4341d3ab2d3ea3c26937c4980ef5157739a
JiangHuYiXiao <1163270704@qq.com>
2019-01-23 20:01:32 +0800

    file

--- day21/glance/__init__.py
+++ day21/glance/__init__.py
@@ -1,10 +1,16 @@
 # print('***********')
 '''
+绝对路径：
 import glance.api
 import glance.cmd
 import glance.db
-'''
+
 # 或者
 from glance import api
 from glance import cmd
 from glance import db
+'''
+# 相对路径：
+from .import api
+from .import cmd
+from .import db
