05f6d36068237e5f81f4955401520e627d31f4ba
JiangHuYiXiao <1163270704@qq.com>
2019-09-23 17:29:45 +0800

    day32

--- day32/struct模块定制报头ftp实战/client.py
+++ day32/struct模块定制报头ftp实战/client.py
@@ -4,30 +4,47 @@
 # @Software       : PythonStudy
 # @Python_verison : 3.7
 # client端读文件后发送文件
+
+import json
 import os
 import socket
 import struct
 
 ss = socket.socket()
 ss.connect(('127.0.0.1',9907))
-file_name = '01 python fullstack s9day33 复习.mp4'
-path1 = 'E:\BaiduNetdiskDownload\Python\老男孩Python\day33'
+file_name = r'01 python fullstack s9day33 复习.mp4'
+path1 = r'E:\BaiduNetdiskDownload\Python\老男孩Python\day33'
 file_path = os.path.join(path1,file_name)
-file_size = os.path.getsize(file_path)
+
 # 定义报头
-head = {'file_name':'01 python fullstack s9day33 复习.mp4','file_size':file_size,'file_type':'mp4','file_path':file_path}
-# 首先发送文件的大小
-file_len_bytes = struct.pack('i',file_size)
-ss.send(file_len_bytes)
-# 读文件
-while True:
-    with open(file_path,mode='rb') as file:
-        file.read()
+head = {'file_name':file_name,'file_size':None,'file_type':'mp4','file_path':file_path}
+# 要发送的文件的大小
+file_size = os.path.getsize(file_path)
+head['file_size'] = file_size
+# 将报头转成字符串进行网络传输
+json_head = json.dumps(head)
+# 字符串转bytes
+bytes_head = json_head.encode('utf-8')
+# 计算head的长度
+head_len = len(bytes_head)
+print(head_len)
+# 使用stuct模块将报头长度转换成4个字节
+pack_len = struct.pack('i',head_len)
+# 发送报头长度
+ss.send(pack_len)
+# 发送报头
+ss.send(bytes_head)
+# 读文件并且发送文件
+with open(file_path,mode='rb') as file:
+    while file_size:
         if file_size >= 1024:
-            file_size = file_size -1024
+            content = ss.send(file.read(1024))
+            file_size = file_size - 1024
+            ss.send(bytes(content))
         else:
-            ss.send()
+            content = file.read(file_size)
+            ss.send(bytes(content))
+            break
 
-
-ss.send()
 ss.close()
+
--- day32/struct模块定制报头ftp实战/server.py
+++ day32/struct模块定制报头ftp实战/server.py
@@ -6,14 +6,33 @@
 # 需求：实现一个大文件的上传或者下载
 # 配置文件 IP地址，端口号
 # server端接收文件后写文件
+
+import json
 import socket
+import struct
 
 ss = socket.socket()
 ss.bind(('127.0.0.1',9907))
 ss.listen()
 conn,addr = ss.accept()
-result = conn.recv(1024)
-
+head_len = conn.recv(4)           # 接收报头的长度
+print(head_len)
+head_len = struct.unpack('i',head_len)[0]
+print(head_len)
+json_head = conn.recv(head_len).decode('utf-8')
+head = json.loads(json_head)   # head字典
+file_size = head['file_size']
+# 先接收文件后写文件
+with open(head['file_name'],mode='wb') as file:
+    while file_size:
+        if file_size >= 1024:
+            content = conn.recv(1024)
+            file.write(content)
+            file_size = file_size - 1024
+        else:
+            content = conn.recv(file_size)
+            file.write(content)
+            break
 
 conn.close()
 ss.close()
