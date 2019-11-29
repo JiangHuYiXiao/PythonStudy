48ffa412140be611276deccc6c7f79604ac87529
JiangHuYiXiao <1163270704@qq.com>
2019-11-29 08:45:39 +0800

    外键变种之一对多&一对一06

--- MySQL/day58/外键变种之一对多&一对一06.py
+++ MySQL/day58/外键变种之一对多&一对一06.py
@@ -48,4 +48,45 @@ VALUES
 
 select * from t1_department;
 select * from t1_user_info;
+
+
+
+-- 外键变种之一对一：
+-- 		博客园的用户表和博客地址表,一个用户只有一个博客地址，注册了博客后就不能再注册了，
+
+
+DROP TABLE
+IF EXISTS t_blog_user;
+
+
+
+CREATE TABLE t_blog_user (
+	id INT auto_increment PRIMARY KEY,
+	user_name VARCHAR (30) NOT NULL,
+	user_id INT NOT null,
+	UNIQUE uq1(user_id)
+) ENGINE = INNODB DEFAULT charset = utf8;
+
+insert into t_blog_user(user_name,user_id) values('jiangtao_jiang',110),('tao_jiang',111),('jiangtao',112),('jianghu',115),('taotao',113),('taobao',114);		-- 唯一索引不允许重复
+insert into t_blog_user(id,user_name,user_id) values(1,'zibaoba',11),(2,'人生',365);			-- 主键不允许重复
+insert into t_blog_user(user_name,user_id) values('jiangtao_jiansfg',110);
+select * from t_blog_user;
+
+
+DROP TABLE
+IF EXISTS t_cnblogs;
+
+CREATE TABLE t_cnblogs (
+	id INT auto_increment PRIMARY KEY,
+	blog_addr VARCHAR (40) NOT NULL,
+	cnblogs_user_id INT NOT NULL,
+	UNIQUE (blog_addr),								-- 唯一索引，表示单独不允许重复
+	UNIQUE (cnblogs_user_id),					-- 唯一索引，表示单独不允许重复
+-- 	UNIQUE uq2 (blog_addr, cnblogs_user_id),	-- 表示两个字段不能完全一样
+
+	CONSTRAINT fk_user_cnblogs FOREIGN KEY (cnblogs_user_id) REFERENCES t_blog_user (user_id)
+) ENGINE = INNODB DEFAULT charset = utf8;
+
+insert into t_cnblogs(blog_addr,cnblogs_user_id) values('yixifsdfu',111),('sdf',112);
+select * from t_cnblogs;
 '''
