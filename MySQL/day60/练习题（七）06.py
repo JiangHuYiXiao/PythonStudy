2476c28996f0c241145ced5e9a7447e149d79106
JiangHuYiXiao <1163270704@qq.com>
2019-12-09 08:54:22 +0800

    练习题(七)06

--- MySQL/day60/练习题（七）06.py
+++ MySQL/day60/练习题（七）06.py
@@ -10,7 +10,9 @@ select score_course_id,max(score_number),min(score_number) from t_score score gr
 
 
 -- 25、按各科平均成绩从低到高和及格率的百分数从高到低顺序；
-
-select score.score_course_id,avg(score.score_number) as 平均成绩 from t_score score group by score.score_course_id order by 平均成绩 dsc;
+-- case when 条件1 then 结果1 else 结果2 end
+select score.score_course_id,avg(score.score_number) as 平均成绩,
+sum(case when score.score_number<60 then 0 else 1 end),sum(1),sum(case when score.score_number<60 then 0 else 1 end)/sum(1) jgl
+from t_score score group by score.score_course_id order by 平均成绩 asc,jgl desc;
 
 '''
