a5773e7b5d67733a5aeb46b2e970080c0ca82bd9
JiangHuYiXiao <1163270704@qq.com>
2019-12-20 10:01:18 +0800

    day47

---
+++ 前端开发/day47/web框架本质02/服务端.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2019/12/20 9:15
+# @Software       : Python_study
+# @Python_verison : 3.7
+
+'''
+
+'''
---
+++ 前端开发/day47/前端内容概述01.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2019/12/20 9:09
+# @Software       : Python_study
+# @Python_verison : 3.7
+
+
+# html 体现于页面的内容
+#     学习标签
+# css  体现于页面外观
+#     学习选择器、属性
+# javascript 体现于页面的交互，动作
+#     学习基础语法 BOM&DOM
