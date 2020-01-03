ee9801357a6034ee983d38d15f89adfb23b5854d
JiangHuYiXiao <1163270704@qq.com>
2020-01-03 09:22:05 +0800

    JS介绍01

---
+++ 前端开发/day51/JS介绍01.js
@@ -0,0 +1,19 @@
+// JS内容：
+// 1、基础语法
+// 2、数据类型
+// 3、函数
+//4、面向对象
+
+//5、BOM  BOM即浏览器对象模型（brower object model）（整合js和浏览器）
+    // BOM包含windows（窗口）、navigator（浏览器）、screen（浏览器屏幕）、history（访问历史）、location（地址）等
+//6、DOM  DOM即文档对象模型（document object model）(整合js，css，html)
+//     // DOM就是把html里的各种数据当做对象处理的一种思路。
+
+// 简单地说，ECMAScript 描述了JavaScript语言本身的相关内容。
+// JavaScript 是脚本语言
+// JavaScript 是一种轻量级的编程语言。
+// JavaScript 是可插入 HTML 页面的编程代码。
+// JavaScript 插入 HTML 页面后，可由所有的现代浏览器执行。
+
+// 7、js引入方式：引入js文件，在head标签内写js代码
+alert('海燕');
