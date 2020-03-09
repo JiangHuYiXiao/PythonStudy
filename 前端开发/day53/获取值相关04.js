09312f4a8d84d80a7828244864075933bdfb92fd
JiangHuYiXiao <1163270704@qq.com>
2020-03-09 09:19:50 +0800

    获取值相关04

---
+++ 前端开发/day53/获取值相关04.js
@@ -0,0 +1,8 @@
+//1、获取HTML中的value可以通过下面的方式获取
+    // elementNode.value
+    // 适用于以下标签：
+    // .input
+    // .select
+    // .textarea
+var schoolEle = document.getElementById("school");
+console.log(schoolEle.value);
