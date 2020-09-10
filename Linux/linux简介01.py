2a39706796c8fa4c6e7985fde3a9f8278d540f47
JiangHuYiXiao <1163270704@qq.com>
2020-09-10 19:10:10 +0800

    Linux

---
+++ Linux/linux简介01.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2020/9/10 18:43
+# @Software       : Python_study
+# @Python_verison : 3.7
+
+# 1、1973年汤姆逊和里奇发布了Unix。
+# 2、塔能鲍姆为了教学使用又开发了一个小型unix称为Minix。
+# 3、李纳斯·托沃兹, 为了将unix应用到开发中使用，编写了Linux内核v-0.02。
+# 4、今天我们大家所熟悉的Ubantu、Redhat、SUSE,Fedora都是由Linux衍生出来的操作系统。
