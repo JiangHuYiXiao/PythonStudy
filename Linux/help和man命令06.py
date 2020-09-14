4a6a985991557fe4253337a96af851693484f842
JiangHuYiXiao <1163270704@qq.com>
2020-09-14 09:11:50 +0800

    Linux

---
+++ Linux/help和man命令06.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2020/9/14 9:08
+# @Software       : Python_study
+# @Python_verison : 3.7
+# 我们在使用Linux命令时候不知道命令有哪些选项时我们可以使用help和man来查看
+# # ls --help
+# # man ls
---
+++ Linux/ls和pwd命令07.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2020/9/14 8:47
+# @Software       : Python_study
+# @Python_verison : 3.7
+# 1、ls
+    # ls：命令默认查找的是当前路径下的非隐藏的文件
+    # ls -a：查看当前路径下的所有文件
+    # ls -l:以长格式显示文件
+    # ls -h：显示文件大小
+    # ls -alh：以长格式显示当前路径下的所有文件和大小
+    # ls home/jianghu ：查看home/jianghu路径下的非隐藏的文件
+    # 通配符* ls 2* ：查看当前路径下以2开头的文件
+
+# 2、pwd
+    # pwd:默认是 Print Working Directory （打印工作目录）的缩写，功能是显示用户当前所处的工作目录，绝对路径
